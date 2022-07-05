71dc98dee6d84fd541727b5bf7cdab143bd25b45
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2022-07-06 06:17:43 +1000

    docs: added docs for smart contracts

--- contracts/TheRealDream.sol
+++ contracts/TheRealDream.sol
@@ -10,6 +10,10 @@ import "@openzeppelin/contracts/token/common/ERC2981.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "./TheRealDreamStorage.sol";
 
+/// @title The Real Dream
+/// @author Mihirsinh Parmar
+/// @notice The real dream is a project to help youtube creators split rewards with NFT holders
+/// @dev It is going to be deployed using proxy
 contract TheRealDream is
     Ownable,
     Pausable,
@@ -18,25 +22,55 @@ contract TheRealDream is
     ERC2981,
     TheRealDreamStorage
 {
-    uint256 public constant PREFIX_MULTIPLIER = 100_000_000;
     using Strings for uint256;
+    
+    /// @notice It is prefix multiplier for reward ID
+    /// @dev each reward as a unique ID, and the token ID for each reward starts with prefix
+    /// @return PREFIX_MULTIPLIER the prefix multipler
+    uint256 public constant PREFIX_MULTIPLIER = 100_000_000;
 
+    /// @notice create log when new payment is released
+    /// @dev emits whenever a user releases a payment
+    /// @param tokenId token ID for which payment was released
+    /// @param payment amount of reward released
+    /// @param receiver receiver address
     event PaymentReleased(
         uint256 indexed tokenId,
         uint256 payment,
         address indexed receiver
     );
+
+    /// @notice create log when creator prepares rewards
+    /// @dev emits whenever owner prepares rewards
+    /// @param depositAmount amount deposited to smart contract for reward
+    /// @param startTime start time when the reward distribution starts
+    /// @param endTime end time when the reward distribution ends
     event PrepareRewards(
         uint256 depositAmount,
         uint256 startTime,
         uint256 endTime
     );
+
+    /// @notice create logs when a new reward asset is added
+    /// @dev emitted when creator adds new reward asset
+    /// @param index index of new reward asset
+    /// @param maximumTokens maximum number of NFTs for this reward
+    /// @param assetURI URI of the reward asset
     event RewardAssetAdded(
         uint256 indexed index,
         uint256 maximumTokens,
         string assetURI
     );
 
+    /// @notice Constructor
+    /// @dev Constructor
+    /// @param _maximumTokens maximum number of NFTs for the initial reward asset
+    /// @param _cooldownPeriod cool down period before new reward distribution can start
+    /// @param _minimumDistributionPeriod minimum time period for which reward distribution can be active
+    /// @param _name name of token
+    /// @param _symbol symbol of token
+    /// @param _baseURI base URI of the NFT metadata
+    /// @param _assetURI asset URI of the new reward asset
     constructor(
         uint256 _maximumTokens,
         uint256 _cooldownPeriod,
@@ -60,6 +94,13 @@ contract TheRealDream is
         );
     }
 
+    /// @notice creator can add new reward asset
+    /// @dev add new reward asset and set reward settings
+    /// @param _maximumTokens maximum number of NFTs for the reward asset
+    /// @param _cooldownPeriod cooldown period before reward distribution starts
+    /// @param _minimumDistributionPeriod minimum time period of reward distribution
+    /// @param _baseURI the base URI of the new asset reward
+    /// @param _assetURI the asset URI of the new reward asset
     function addNewReward(
         uint256 _maximumTokens,
         uint256 _cooldownPeriod,
@@ -82,6 +123,14 @@ contract TheRealDream is
         );
     }
 
+    /// @notice creator can add new reward asset
+    /// @dev add new reward asset and set reward settings
+    /// @param _index new index of the reward asset
+    /// @param _maximumTokens maximum number of NFTs for the reward asset
+    /// @param _cooldownPeriod cooldown period before reward distribution starts
+    /// @param _minimumDistributionPeriod minimum time period of reward distribution
+    /// @param _baseURI the base URI of the new asset reward
+    /// @param _assetURI the asset URI of the new reward asset
     function _addNewReward(
         uint256 _index,
         uint256 _maximumTokens,
@@ -98,16 +147,22 @@ contract TheRealDream is
         emit RewardAssetAdded(_index, _maximumTokens, _assetURI);
     }
 
-    //
+    /// @notice pause smart contract
+    /// @dev pauses the functionality to release reward
     function pause() external onlyOwner whenNotPaused {
         _pause();
     }
 
+    /// @notice unpause smart contract
+    /// @dev unpauses the functionality to release reward
     function unpause() external onlyOwner whenPaused {
         _unpause();
     }
 
-    // Owner Functions
+    /// @notice airdrop NFTs for a reward index
+    /// @dev airdrop tokens for a reward index to list of addresses
+    /// @param _receivers list of addresses to sirdrop token
+    /// @return _rewardIndex reward index whose tokens will be airdropped
     function airdrop(address[] calldata _receivers, uint256 _rewardIndex)
         external
         onlyOwner
@@ -135,14 +190,25 @@ contract TheRealDream is
         }
     }
 
+    /// @notice set royalty details
+    /// @dev set default royalties
+    /// @param _account address of royalty receiver
+    /// @param _fee royalty percentage
     function setRoyalty(address _account, uint96 _fee) external onlyOwner {
         _setDefaultRoyalty(_account, _fee);
     }
 
+    /// @notice remove royalty
+    /// @dev remove royalty
     function removeRoyalty() external onlyOwner {
         _deleteDefaultRoyalty();
     }
 
+    /// @notice creator can deposit and set new distribution reward
+    /// @dev creator deposits and set new distribution reward
+    /// @param _distributionStartTime distribution of reward start time
+    /// @param _distributionEndTime distribution of reward end time
+    /// @param _rewardIndex reward asset index whose distribution period needs to start
     function prepareForRewards(
         uint256 _distributionStartTime,
         uint256 _distributionEndTime,
@@ -173,6 +239,7 @@ contract TheRealDream is
         );
     }
 
+    /// @inheritdoc	ERC721
     function supportsInterface(bytes4 interfaceId)
         public
         view
@@ -183,6 +250,10 @@ contract TheRealDream is
         return super.supportsInterface(interfaceId);
     }
 
+    /// @notice set new base URI for reward asset
+    /// @dev sets new base URI for a reward asset
+    /// @param _baseURI new base URI
+    /// @param _rewardIndex reward asset index
     function setBaseURI(string calldata _baseURI, uint256 _rewardIndex)
         external
         onlyOwner
@@ -190,6 +261,9 @@ contract TheRealDream is
         rewards[_rewardIndex].baseURI = _baseURI;
     }
 
+    /// @notice release reward
+    /// @dev release reward
+    /// @param tokenId token ID for what rewards are released
     function releaseReward(uint256 tokenId) external whenNotPaused {
         uint256 _rewardIndex = getRewardIndex(tokenId);
         require(
@@ -214,6 +288,10 @@ contract TheRealDream is
         emit PaymentReleased(tokenId, payment, ownerOf(tokenId));
     }
 
+    /// @notice calculate pending reward for a token ID
+    /// @dev calculate pending reward for a token ID
+    /// @param tokenId token ID for which pending reward was calculated
+    /// @return pendingAmount amount fo pending rewards
     function pendingReward(uint256 tokenId) external view returns (uint256) {
         uint256 _rewardIndex = getRewardIndex(tokenId);
         uint256 totalReceived = rewards[_rewardIndex].totalBalance +
@@ -223,6 +301,7 @@ contract TheRealDream is
             rewards[_rewardIndex].released[tokenId];
     }
 
+    /// @inheritdoc	ERC721
     function tokenURI(uint256 tokenId)
         public
         view
@@ -241,14 +320,23 @@ contract TheRealDream is
                 : "";
     }
 
+    /// @notice increases balance stored in state
+    /// @dev increases balance stored in state
+    /// @param _value amount by which balance should be increased
+    /// @param _rewardIndex reward asset index
     function _increaseBalance(uint256 _value, uint256 _rewardIndex) internal {
         rewards[_rewardIndex].totalBalance += _value;
     }
 
+    /// @notice decreseas balance stored in state
+    /// @dev decreseas balance stored in state
+    /// @param _value amount by which balance should be decreased
+    /// @return _rewardIndex reward asset index
     function _decreaseBalance(uint256 _value, uint256 _rewardIndex) internal {
         rewards[_rewardIndex].totalBalance -= _value;
     }
 
+    /// @inheritdoc	ERC721
     function _beforeTokenTransfer(
         address from,
         address to,
@@ -261,13 +349,18 @@ contract TheRealDream is
                 rewards[_rewardIndex].distributionEndTime > block.timestamp),
             "TRANSFER_PAUSED"
         );
-        // }
     }
 
+    /// @notice get reward index for a token ID
+    /// @dev for a specified token ID, it calculates the token ID
+    /// @param _tokenId token ID for which reward index should be calculated
+    /// @return rewardIndex index of reward asset
     function getRewardIndex(uint256 _tokenId) public pure returns (uint256) {
         return _tokenId / PREFIX_MULTIPLIER;
     }
 
+    /// @notice fallback method to revert when any ETH is transferred
+    /// @dev fallback method to revert when any ETH is transferred
     receive() external payable {
         revert("NOT_ALLOWED");
     }
--- contracts/TheRealDreamStorage.sol
+++ contracts/TheRealDreamStorage.sol
@@ -1,5 +1,9 @@
 pragma solidity 0.8.13;
 
+/// @title The Real Dream Storage
+/// @author Mihirsinh Parmar
+/// @notice Storage contract of The Real Dream
+/// @dev A separate storage contract to prevent storage collision in proxy
 contract TheRealDreamStorage {
     struct RewardData {
         uint256 maximumTokens; // maximum amount of tokens that can be minted //set by owner
@@ -14,7 +18,17 @@ contract TheRealDreamStorage {
         string assetURI; // set by owner
         mapping(uint256 => uint256) released;
     }
+
+    /// @notice mapping of reward index with reward data
+    /// @return rewards returns the reward data struct
     mapping(uint256 => RewardData) public rewards;
+
+    /// @notice next reward index
+    /// @return rewardsIndex next reward index
     uint256 public rewardsIndex;
+
+    /// @notice total supply
+    /// @dev total supply
+    /// @return totalSupply total supply
     uint256 public totalSupply;
 }
